89c60a9012319e809353e33562676c6df851d19d
BishalKhatri404 <khatrybishal20@gmail.com>
2025-11-19 12:22:04 +0545

    task2 completed

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from task2");
+
+            Calculator calculator = new Calculator();
+
+            calculator.PrintWelcome();
+
+            int sum = calculator.Add(10, 20);
+            Console.WriteLine($"Sum: {sum}");
+
+            int product = calculator.Multiply(10);
+            Console.WriteLine($"Product: {product}");
+
+
+        }
+    }
+}
