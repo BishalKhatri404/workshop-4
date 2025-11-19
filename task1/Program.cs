c64c2303ee3566d3865f119975876a7d766dad17
BishalKhatri404 <khatrybishal20@gmail.com>
2025-11-19 11:58:51 +0545

    task1 completed

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Student s1 = new Student();
+            Student s2 = new Student(); 
+
+            s1.student_id = 23;
+            s1.student_name = "Sudan";
+            s1.student_age = 16;
+
+            s2.student_id = 24;
+            s2.student_name = "Aship";
+            s2.student_age = 17;
+
+            Console.WriteLine($"Student 1 details:\n{s1.student_id}\n{s1.student_name}\n{s1.student_age}");
+            Console.WriteLine($"Student 2 details:\n{s2.student_id}\n{s2.student_name}\n{s2.student_age}");
+
+        }
+    }
+}
---
+++ task1/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Student
+    {
+        public int student_id;
+        public string student_name;
+        public int student_age;
+
+        public static string student_college = "IIC college";
+    }
+}
